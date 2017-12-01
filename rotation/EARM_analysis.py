702220122d0816e12e2fc098ce7c475a3627390c
blynn7567 <rebecca.l.weiner@vanderbilt.edu>
2017-12-01 13:52:09 -0600

    changed sim to sim_results

--- rotation/EARM_analysis.py
+++ rotation/EARM_analysis.py
@@ -1,6 +1,13 @@
 from time_death import *
 from pysb.simulator.base import SimulationResult
 
-sim = SimulationResult.load('EARM_Simulations1k.h5')
+sim_result = SimulationResult.load('EARM_Simulations1k.h5')
 
-
+# cPARP_obs = [2]sim.observables
+# sim.species = species
+#
+#
+# functions =
+# species = species
+# xdata = tspan
+# ydata = cPARP_obs
