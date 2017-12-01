0daf66355f435b717e4eeb2ab8a967c6530f65f7
blynn7567 <rebecca.l.weiner@vanderbilt.edu>
2017-12-01 15:10:51 -0600

    first pass at making a loop through Td function

--- rotation/EARM_analysis.py
+++ rotation/EARM_analysis.py
@@ -1,13 +1,20 @@
 from time_death import *
 from pysb.simulator.base import SimulationResult
+import matplotlib.pyplot as plt
+import nump as np
 
 sim_result = SimulationResult.load('EARM_Simulations1k.h5')
+tspan = np.linspace(0, 20000, 100)
 
-# cPARP_obs = [2]sim.observables
-# sim.species = species
-#
-#
-# functions =
-# species = species
-# xdata = tspan
-# ydata = cPARP_obs
+list_results = [0] * 1000
+
+for sim_array in sim_result.observables:
+    observable = sim_array['cPARP']
+    xdata = tspan
+    functions = curve_fit(sig_apop(),'cPARP',xdata,sim_array)
+    list.append(list_results)
+
+plt.hist(list_results,bin='auto')
+plt.title("Histogram 1ksim Time of Death")
+plt.save('Histogram_1ksim Time of Death')
+
