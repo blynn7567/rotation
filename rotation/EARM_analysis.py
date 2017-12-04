278dd9060b9a3fe4b2c6d0396b1157a750cd7611
blynn7567 <rebecca.l.weiner@vanderbilt.edu>
2017-12-04 14:15:58 -0600

    chanegd cPARP-->observable (un-did)

--- rotation/EARM_analysis.py
+++ rotation/EARM_analysis.py
@@ -15,7 +15,7 @@ for sim_array in sim_result.observables:
     max_value = max(non_norm_observable)
     observable = non_norm_observable/max_value
     xdata = tspan
-    time_death = curve_fit_ftn(sig_apop,observable,xdata,sim_array,**{'p0': [100, 100, 100]})
+    time_death = curve_fit_ftn(sig_apop,'cPARP',xdata,sim_array,**{'p0': [100, 100, 100]})
     list_results.append(time_death)
 
 plt.hist(list_results,bins='auto')
