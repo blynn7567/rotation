d08fc554260a02ccd15ddd8ab17b21e8a4dde434
blynn7567 <rebecca.l.weiner@vanderbilt.edu>
2017-12-06 09:46:15 -0600

    analyzing 1k Td with tuple creation

--- rotation/EARM_analysis.py
+++ rotation/EARM_analysis.py
@@ -5,7 +5,7 @@ from pysb.simulator.base import SimulationResult
 import matplotlib.pyplot as plt
 import numpy as np
 
-sim_result = SimulationResult.load('EARM_Simulations20k12_5.h5')
+sim_result = SimulationResult.load('EARM_Simulations1k.h5')
 tspan = np.linspace(0, 20000, 100)
 
 list_results = []
@@ -16,13 +16,22 @@ for sim_array in sim_result.observables:
     time_death = curve_fit_ftn(sig_apop,'cPARP',xdata,sim_array,**{'p0': [100, 100, 100]})
     list_results.append(time_death)
 
-time_death_filt = [x for x in list_results if x>0 and x<20000]
-print(time_death_filt)
-plt.hist(time_death_filt,bins=350)
-y = mlab.normpdf(bins, mu, sigma)
-plt.plot(bins, y, 'r--')
-plt.xlabel('time(s)')
-plt.ylabel('molecules cPARP')
-plt.title("Time of Death")
-plt.savefig('Histogram_20ksim12_5_td')
+#creating list of tuples for Td:parameter idx
+param_idx = list(range(999))
+tuple_list = zip(list_results,param_idx)
+
+#time_death_filt = [x for x in list_results if x>0 and x<20000]
+tuple_list_filt = [x for x in list_results if x>0 and x<20000]
+tuple_list_filt.sort(key=lambda x: x[0])
+slowest = tuple_list_filt[0:99]
+fastest = tuple_list_filt[901:1000]
+
+#print(time_death_filt)
+#plt.hist(time_death_filt,bins=25)
+#y = mlab.normpdf(bins, mu, sigma)
+#plt.plot(bins, y, 'r--')
+#plt.xlabel('time(s)')
+#plt.ylabel('cell count')
+#plt.title("Time of Death")
+#plt.savefig('Histogram_20ksim12_5_td')
 
